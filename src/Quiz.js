5ee2b1876346411e94854fc784e2ab1d57e40096
ioanh <ioan.xristov@abv.bg>
2021-09-06 01:03:50 +0300

    making the logic stronger with each hour xD almost there

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,44 @@
 import React, { Component, useState, useRef } from 'react'
 
 export default class Quiz extends Component {
-    state = {count: 100};
-    intervalId;
+    state = {
+        timerCount: 60,
+        question: '',
+        answers: [],
+    };
+    timerInterval;
+    questionInterval;
+    quizQuestions = [{question: 'What is this?', answers: ['Answer1', 'Answer2']}, {question: 'What is that?', answers: ['Answer3', 'Answer4']}]
 
     componentDidMount(){
-         this.intervalId = setInterval(() => {
+        this.setState(() => {
+            return {
+                question: this.quizQuestions[0].question,
+                answers: this.quizQuestions[0].answers
+            }
+        })
+
+        this.questionInterval = setInterval(() => {
+            this.setState(() => {
+                return {
+                    question: this.quizQuestions[1].question,
+                    answers: this.quizQuestions[1].answers,
+                    timerCount: 60
+                }
+            })
+        }, 60000)
+
+         this.timerInterval = setInterval(() => {
             this.setState(prevState => {
                 return {
-                    count: prevState.count - 1,
+                    timerCount: prevState.timerCount - 1,
                 }
             })
         }, 1000)
     }
 
     cancelInt = () => {
-        clearInterval(this.intervalId)
+        clearInterval(this.timerInterval)
     }
 
     resetInt = () => {
@@ -30,10 +53,10 @@ export default class Quiz extends Component {
         return (
         <div id="wrapper">
             <div class="quiz">
-                <progress value={this.state.count} max="100"></progress>
-                <p onClick={this.resetInt}>What is this?</p>
+                <progress value={this.state.timerCount} max="60"></progress>
+                <p onClick={this.resetInt}>{this.state.question}</p>
                 <ol type="A">
-                    <li>This is the answer</li>
+                    <li>{this.state.answers[0]}</li>
                 </ol>
             </div>
         </div>
